978efc4aace836292d462cce54366ad226bcad10
Coder-Mehedi <mehedi5051@gmail.com>
2020-01-23 17:25:34 +0600

    set current, update contact done using redux

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -3,7 +3,10 @@ import {
   SET_LOADING,
   LOGS_ERROR,
   ADD_LOG,
-  DELETE_LOG
+  UPDATE_LOG,
+  DELETE_LOG,
+  SET_CURRENT,
+  CLEAR_CURRENT
 } from "./types";
 
 // Get logs from server
@@ -67,6 +70,47 @@ export const deleteLog = id => async dispatch => {
       payload: err.response.data
     });
   }
+};
+
+// Update logs on server
+export const updateLog = log => async dispatch => {
+  try {
+    setLoading();
+    const res = await fetch(`/logs/${log.id}`, {
+      method: "PUT",
+      body: JSON.stringify(log),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+
+    const data = await res.json();
+
+    dispatch({
+      type: UPDATE_LOG,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data
+    });
+  }
+};
+
+// Set Current log
+export const setCurrent = log => {
+  return {
+    type: SET_CURRENT,
+    payload: log
+  };
+};
+
+// Clear Current Log
+export const clearCurrent = () => {
+  return {
+    type: CLEAR_CURRENT
+  };
 };
 
 export const setLoading = () => {
